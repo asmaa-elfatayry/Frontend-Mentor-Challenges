469a4ee018a2f7726f17f431bba8ff243d0ad916
asmaa-elfatayry <asmaa-elfatayry@github.com>
2022-11-17 19:57:11 +0200

    add project

---
+++ junior/blogr-landing-page-main/js/index.js
@@ -0,0 +1,50 @@
+console.log(window.innerWidth);
+let navs = document.querySelectorAll(".head-nav ");
+let menue = document.querySelector(".toggle-munue");
+let close_menue = document.querySelector(".toggle-close");
+let navTog = document.querySelector(".bloger-nav");
+navs.forEach((nav) => {
+  nav.addEventListener("click", () => {
+    // remove all style first
+    navs.forEach((check) => {
+      check.nextElementSibling.style.display = "none";
+      check.style.textDecoration = "none";
+      nav.childNodes[1].style.transform = "rotate(0deg)";
+    });
+    nav.style.textDecoration = "underline";
+
+    nav.nextElementSibling.style.display = "block";
+
+    nav.childNodes[1].style.transform = "rotate(180deg)";
+  });
+
+  nav.nextElementSibling.addEventListener("mouseleave", () => {
+    nav.nextElementSibling.style.display = "none";
+    nav.style.textDecoration = "none";
+    nav.childNodes[1].style.transform = "rotate(0deg)";
+  });
+});
+
+menue.addEventListener("click", () => {
+  navs.forEach((nav) => {
+    nav.childNodes[1].src = "../images/icon-arrow-dark.svg";
+    nav.addEventListener("click", () => {
+      nav.nextElementSibling.style.backgroundColor = "rgb(239,239,241)";
+    });
+  });
+  menue.style.display = "none";
+  close_menue.style.display = "flex";
+  close_menue.style.cursor = "pointer";
+  navTog.classList.add("toggle-bloger-nav");
+});
+
+close_menue.addEventListener("click", () => {
+  menue.style.display = "flex";
+  close_menue.style.display = "none";
+  navTog.classList.remove("toggle-bloger-nav");
+});
+
+if (document.innerWidth > 375) {
+  menue.style.display = "none";
+  close_menue.style.display = "none";
+}
