00d2cae9088f61e2950c47dd7b00979433e319a3
asmaa-elfatayry <asmaa-elfatayry@github.com>
2022-11-19 18:58:44 +0200

    dropdown navigation project

---
+++ junior/intro-section-with-dropdown-navigation-main/README.md
@@ -0,0 +1,25 @@
+# Frontend Mentor - Intro section with dropdown navigation
+
+![Design preview for the Intro section with dropdown navigation coding challenge](./design/desktop-preview.jpg)
+
+## Welcome! 👋
+
+- Solution URL: https://github.com/asmaa-elfatayry/Frontend-Mentor-Challenges/tree/main/junior/
+- Live Site URL: https://asmaa-elfatayry.github.io/Frontend-Mentor-Challenges/junior/
+
+### Built with
+
+- Semantic HTML5 markup
+- SCSS custom properties
+- JS
+- respnsive in mobile screen
+
+### What I learned
+
+- navagation drop
+- daynamic web
+
+## Author
+
+- Website - [Asmaa-elfatayry](https://github.com/asmaa-elfatayry)
+- Frontend Mentor - [@asmaa-elfatayry](https://www.frontendmentor.io/profile/asmaa-elfatayry)
---
+++ junior/intro-section-with-dropdown-navigation-main/js/index.js
@@ -0,0 +1,50 @@
+const nav_drops = document.querySelectorAll(".drop");
+const menue = document.querySelector(".toggle-menue");
+const close = document.querySelector(".toggle-close");
+const nav = document.querySelector("nav");
+
+nav_drops.forEach((drop) => {
+  drop.addEventListener("click", () => {
+    // reseat the style
+    nav_drops.forEach((res) => {
+      res.nextElementSibling.nextElementSibling.style.display = "none";
+      drop.nextElementSibling.src = "../images/icon-arrow-down.svg";
+    });
+    // drop the menue
+    drop.nextElementSibling.nextElementSibling.style.display = "block";
+    drop.nextElementSibling.src = "../images/icon-arrow-up.svg";
+  });
+});
+
+nav_drops.forEach((drop) => {
+  drop.nextElementSibling.nextElementSibling.addEventListener(
+    "mouseleave",
+    () => {
+      // drop the menue
+      drop.nextElementSibling.nextElementSibling.style.display = "none";
+      drop.nextElementSibling.src = "../images/icon-arrow-down.svg";
+    }
+  );
+});
+
+// show nav in mobile
+menue.addEventListener("click", () => {
+  menue.style.display = "none";
+  close.style.display = "flex";
+  nav.classList.add("shownav");
+});
+
+close.addEventListener("click", () => {
+  close.style.display = "none";
+  menue.style.display = "flex";
+  nav.classList.remove("shownav");
+});
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 375) {
+    menue.style.display = "none";
+    close.style.display = "none";
+  } else {
+    menue.style.display = "flex";
+  }
+});
